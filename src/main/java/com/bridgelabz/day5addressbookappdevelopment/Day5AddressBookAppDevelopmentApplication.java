2573072d45ff990be90f6b9170761c8fe9cd7e61
NEETAMOURYA <neeta.mourya@bridgelabz.com>
2022-05-09 19:21:02 +0530

    Added: Create a Address Book Spring Project to cater to REST Request from Address Book UI

---
+++ src/main/java/com/bridgelabz/day5addressbookappdevelopment/Day5AddressBookAppDevelopmentApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.day5addressbookappdevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day5AddressBookAppDevelopmentApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Spring Address Book App Development");
+        SpringApplication.run(Day5AddressBookAppDevelopmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bridgelabz/day5addressbookappdevelopment/Day5AddressBookAppDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.day5addressbookappdevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day5AddressBookAppDevelopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
